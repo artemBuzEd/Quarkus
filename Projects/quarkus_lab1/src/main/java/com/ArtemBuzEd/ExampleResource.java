0c2dbf6f04f04abe89642e65b2a1c646e8d24bad
artemBuzEd <buziak.artem@chnu.edu.ua>
2025-09-13 23:30:09 +0300

    Tested simple securing of an app with OpenId Connect (Keycloak) and Quarkus Dev Services

--- Projects/quarkus_lab1/src/main/java/com/ArtemBuzEd/ExampleResource.java
+++ Projects/quarkus_lab1/src/main/java/com/ArtemBuzEd/ExampleResource.java
@@ -3,7 +3,11 @@ package com.ArtemBuzEd;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.SecurityContext;
+
+import java.security.Principal;
 
 @Path("/hello")
 public class ExampleResource {
@@ -12,5 +16,16 @@ public class ExampleResource {
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Hello from Quarkus REST";
+    }
+
+    @GET
+    @Path("/whoami")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String whoami(@Context SecurityContext securityContext) {
+        Principal userPrincipal = securityContext.getUserPrincipal();
+        if(userPrincipal != null) {
+            return userPrincipal.getName();
+        }
+        return "anonymous";
     }
 }
